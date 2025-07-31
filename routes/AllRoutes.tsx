f3435402f3101565f575e51d39d1e24f2404864c
Bassel Bitar <91824524+basselbitar@users.noreply.github.com>
2025-07-31 12:11:18 +0100

    Page5 Footers

--- routes/AllRoutes.tsx
+++ routes/AllRoutes.tsx
@@ -3,16 +3,19 @@ import { Home } from "../pages/Home";
 import { Page2 } from "../pages/Page2";
 import Page3 from "../pages/Page3";
 import { Page4 } from "../pages/Page4";
+import { Page5 } from "../pages/Page5";
 
 export const AllRoutes = () => {
   return (
-  <>
-    <Routes>
+    <>
+      <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="page2" element={<Page2/>} />
+        <Route path="page2" element={<Page2 />} />
         <Route path="page3" element={<Page3 />} />
         <Route path="page4" element={<Page4 />} />
-    </Routes>
-  </>
-  )
-}
+        <Route path="page5" element={<Page5 />} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </>
+  );
+};
